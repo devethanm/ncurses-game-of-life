b9428ef5f49e67dc1637ac69ab0171cf13e92295
Ethan M <ethanmdev@gmail.com>
2021-01-08 16:48:59 -0700

    for loop with char printing

--- main.c
+++ main.c
@@ -2,10 +2,23 @@
 #include <stdio.h>
 
 int main() {
+	int ch;
 
+	// init functions
 	initscr();
-	printw("Hello World");
-	refresh();
+	raw();
+	noecho();
+
+	printw("type any character, press q to quit\n");
+	while( ch != 'q') {	
+		ch = getch();
+	
+		addch(ch);
+		refresh();
+	}
+		
+	// These last two lines are just a way to keep the program open,
+	// so it doesnt disapear instantly after running
 	getch();
 	endwin();
 
